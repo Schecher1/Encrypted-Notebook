19cb7a1d82d8f21749e7475143677f959cd59820
Schecher_1 <Schecher_1@web.de>
2022-03-23 20:58:08 +0100

    Removed unnecessary references

--- WPF-Encrypted-Notebook/Pages/PageServerLogin.xaml.cs
+++ WPF-Encrypted-Notebook/Pages/PageServerLogin.xaml.cs
@@ -1,21 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using LIB_Encrypted_Notebook;
 using LIB_Encrypted_Notebook.Database;
-using LIB_Encrypted_Notebook.DataModels;
 using WPF_Encrypted_Notebook.Classes;
 
 namespace WPF_Encrypted_Notebook.Pages
